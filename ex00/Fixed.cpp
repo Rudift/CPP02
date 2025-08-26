a115771ed4d554a0acc94072e357bb9c07ee4824
Valentin Deliere <vdeliere@f6r7s12.clusters.42paris.fr>
2025-08-26 14:03:06 +0200

    push n2

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -12,3 +12,32 @@
 
 #include "Fixed.hpp"
 
+//Constructeur
+Fixed::Fixed(void): _value(0){
+
+}
+
+//Constructeur de copie
+Fixed::Fixed(const Fixed& other):_value(other._value){
+
+}
+
+//Surcharge de l'operateur d'affectation
+
+
+//Destructeur
+Fixed::~Fixed(void){
+	
+}
+
+//Fonctions Membres :
+
+int	Fixed::getRawBits(void) const{
+
+}
+
+void	Fixed::setRawBits(int const raw){
+
+}
+
+
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -12,6 +12,21 @@
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+#  include <string>
+#  include <iostream>
 
+
+class Fixed{
+	private:
+		int	_value;
+		static int const	_nb_bits = 8;
+	public:
+		Fixed(void);
+		Fixed(const Fixed& other);
+		~Fixed(void);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
 
 #endif
